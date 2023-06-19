8016857a20e6ea3c8bf9c92e5e64624e4b9406b9
Jeffrey Li <jeffreyli8000@gmail.com>
2023-06-18 22:15:10 -0400

    profile

---
+++ pages/settings.tsx
@@ -0,0 +1,11 @@
+import Profile from "@/components/Dashboard/SidebarContents/Profile";
+
+export default function settings(){
+   return(
+      <>
+      <h1 className="text-3xl font-bold">My Profile</h1>
+      <div className="h-5"/>
+      <Profile/>
+      </>
+   )
+}
